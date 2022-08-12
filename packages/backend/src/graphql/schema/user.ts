af1105a5562bc42ef09c364eb926cb3aa2986b8e
Will Harris <will.t.harris@protonmail.com>
2022-08-11 19:45:06 -0700

    wip

--- packages/backend/src/graphql/schema/user.ts
+++ packages/backend/src/graphql/schema/user.ts
@@ -1,14 +1,11 @@
-import { asNexusMethod, nonNull, objectType } from "nexus";
-import { GraphQLDate } from "graphql-iso-date";
-
-asNexusMethod(GraphQLDate as any, "date");
+import { nonNull, objectType } from "nexus";
 
 export const User = objectType({
   name: "User",
   definition(t) {
     t.id("id");
-    t.field("createdAt", { type: nonNull("date") });
-    t.field("updatedAt", { type: nonNull("date") });
+    // t.field("createdAt", { type: nonNull("date") });
+    // t.field("updatedAt", { type: nonNull("date") });
     t.field("email", { type: nonNull("string") });
     t.string("name");
     t.field("password", { type: nonNull("string") });
